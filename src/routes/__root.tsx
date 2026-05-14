846be77dc7ff1eb89e38333f53e608a037a1b263
200backflips <fredrikkarlsson320@gmail.com>
2026-05-15 00:00:01 +0200

    add buttons

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,6 +1,6 @@
 import { Link, Outlet, createRootRoute } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
-import { CogIcon } from "lucide-react";
+import { CircleQuestionMark, MoonIcon } from "lucide-react";
 
 export const Route = createRootRoute({
   component: RootComponent,
@@ -10,11 +10,14 @@ function RootComponent() {
   return (
     <>
       <div className="p-6 h-screen flex flex-col gap-6 bg-background">
-        <div className="relative flex items-center justify-center">
-          <Link to="/" className="absolute left-0">
-            <CogIcon className="size-5" />
+        <div className="flex items-center justify-between">
+          <Link to="/">
+            <CircleQuestionMark className="size-5" />
           </Link>
           <h2 className="text-center">Nation Spin</h2>
+          <button>
+            <MoonIcon className="size-5" />
+          </button>
         </div>
         <Outlet />
       </div>
