cf9b8ff7a633623ec84c3af77e4ffd2988f61de6
200backflips <fredrikkarlsson320@gmail.com>
2026-05-15 16:15:02 +0200

    add footer

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,5 +1,4 @@
 import { Outlet, createRootRoute } from "@tanstack/react-router";
-import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
 import { MoonIcon, SunIcon } from "lucide-react";
 import GameInstructions from "@/components/game-instructions";
 import { useState } from "react";
@@ -8,31 +7,37 @@ export const Route = createRootRoute({
   component: RootComponent,
 });
 
-function RootComponent() {
+function ThemeToggle() {
   const [theme, setTheme] = useState("light");
 
   return (
-    <>
-      <div className="p-6 h-screen flex flex-col gap-8 bg-background">
-        <div className="flex items-center justify-between">
-          <GameInstructions />
-          <h2 className="text-center">Nation Spin</h2>
-          <button
-            onClick={() => {
-              document.documentElement.classList.toggle("dark");
-              setTheme(theme === "dark" ? "light" : "dark");
-            }}
-          >
-            {theme === "dark" ? (
-              <MoonIcon className="size-5" />
-            ) : (
-              <SunIcon className="size-5" />
-            )}
-          </button>
-        </div>
-        <Outlet />
+    <button
+      onClick={() => {
+        document.documentElement.classList.toggle("dark");
+        setTheme(theme === "dark" ? "light" : "dark");
+      }}
+    >
+      {theme === "dark" ? (
+        <MoonIcon className="size-5" />
+      ) : (
+        <SunIcon className="size-5" />
+      )}
+    </button>
+  );
+}
+
+function RootComponent() {
+  return (
+    <div className="p-6 h-screen flex flex-col gap-8 bg-background">
+      <div className="flex items-center justify-between">
+        <GameInstructions />
+        <h2 className="text-center">Nation Spin</h2>
+        <ThemeToggle />
       </div>
-      <TanStackRouterDevtools />
-    </>
+      <Outlet />
+      <p className="text-gray-400 text-xs text-center">
+        © 2026 Gus Davidson Group
+      </p>
+    </div>
   );
 }
