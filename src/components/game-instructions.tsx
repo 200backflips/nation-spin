22ee4c32c277237d7b0ae9d9cc678c511f625818
200backflips <fredrikkarlsson320@gmail.com>
2026-05-16 17:47:49 +0200

    update instructions

--- src/components/game-instructions.tsx
+++ src/components/game-instructions.tsx
@@ -7,6 +7,22 @@ import {
   DialogTrigger,
 } from "@/components/ui/dialog";
 import { CircleQuestionMarkIcon } from "lucide-react";
+import { Separator } from "./ui/separator";
+
+const points = [
+  {
+    number: 3,
+    order: "first",
+  },
+  {
+    number: 2,
+    order: "second",
+  },
+  {
+    number: 1,
+    order: "third",
+  },
+];
 
 export default function GameInstructions() {
   return (
@@ -17,8 +33,33 @@ export default function GameInstructions() {
       <DialogContent className="sm:max-w-sm" aria-describedby={undefined}>
         <DialogHeader>
           <DialogTitle>How to play</DialogTitle>
-          <DialogDescription>Yackety smackety, rules go here</DialogDescription>
         </DialogHeader>
+
+        <DialogDescription>
+          Nation Spin is a team-based, word association game.
+        </DialogDescription>
+        <DialogDescription>
+          For each turn, one member will give a single word clue about the
+          country on the screen. Your team scores points based on how many clues
+          the other members need to guess the correct country.
+        </DialogDescription>
+        <Separator />
+        <h4>Points system</h4>
+        <div className="flex flex-col gap-2">
+          {points.map((point) => (
+            <div
+              key={point.number}
+              className="flex items-center justify-start gap-2 p-4 border rounded-md bg-accent"
+            >
+              <p className="size-8 flex items-center justify-center bg-primary text-white rounded-full">
+                {point.number}
+              </p>
+              <p className="text-base">
+                For guessing right on the <strong>{point.order}</strong> clue
+              </p>
+            </div>
+          ))}
+        </div>
       </DialogContent>
     </Dialog>
   );
